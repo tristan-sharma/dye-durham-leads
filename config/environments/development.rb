a3f8ef73dbf704cbaea9836a02475faab63e5059
“Tristan <tristansharma@gmail.com>
2019-10-09 19:30:48 -0400

    Rm pw

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,14 +11,7 @@ config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default_options = {from: 'tristansharma@gmail.com'}
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-address:              'smtp.gmail.com',
-port:                 587,
-user_name:            'tristansharma',
-password:             'Bailey630/$',
-authentication:       'plain',
-enable_starttls_auto: true  }
+
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
