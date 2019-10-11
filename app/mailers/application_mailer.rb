c4c5fc2a2906efe18780198532123d6a46fbc0c7
“Tristan <tristansharma@gmail.com>
2019-10-11 15:17:42 -0400

    Mailgun default

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'tristansharma@gmail.com'
+  default from: 'tristansharma@sandboxbdc57fe4809b43b5b3676de42ccd5449.mailgun.org'
   layout 'mailer'
 end
